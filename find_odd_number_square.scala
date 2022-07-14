34b86800d95e2407fbfc998e265a86ae0bbd88b2
yluoc <98978059+yluoc@users.noreply.github.com>
2022-07-13 21:28:03 -0400

    Create find_odd_number_square.scala

---
+++ find_odd_number_square.scala
@@ -0,0 +1,16 @@
+/**
+this function is used to make an integer list into a list which only contains odd number without using "for loop" or "if, else"
+*/
+object question_2 {
+  def find_odd_square(list: List[Int])={
+    val odd_list: List[Int]= list.filter(_ %2 !=0)
+    val odd_square: List[Double]= odd_list.map(Math.pow(_, 2))
+    val sum_odd: Int= odd_square.sum.toInt
+    print(sum_odd)
+  }
+
+  def main(args:Array[String])={
+    val list1=List(1,2,3,4,5,6,7)
+    find_odd_square(list1)
+  }
+}
